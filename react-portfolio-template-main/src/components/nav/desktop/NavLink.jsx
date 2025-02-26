99ef6c60959cc7b256915063fcbf7c2701c0b4e3
Awais Amjad <2022cs541@student.uet.edu.pk>
2025-02-26 17:27:45 +0500

    Update NavLink.jsx

--- react-portfolio-template-main/src/components/nav/desktop/NavLink.jsx
+++ react-portfolio-template-main/src/components/nav/desktop/NavLink.jsx
@@ -1,26 +1,36 @@
-import "./NavLink.scss"
-import React from 'react'
-import MenuItem from "/src/components/generic/MenuItem.jsx"
-import {useUtils} from "/src/helpers/utils.js"
-import SensitiveButton from "/src/components/generic/SensitiveButton.jsx"
+import "./NavLink.scss";
+import React from "react";
+import MenuItem from "/src/components/generic/MenuItem.jsx";
+import { useUtils } from "/src/helpers/utils.js";
+import SensitiveButton from "/src/components/generic/SensitiveButton.jsx";
 
 function NavLink({ shrink, label, icon, size, className, rounded, selected, disabled, onClick, tooltip }) {
-    const utils = useUtils()
+    const utils = useUtils();
+
+    // Hide "Updates" link
+    if (label === "Updates") {
+        return null; // Prevent rendering
+    }
 
     return (
-        <SensitiveButton className={`nav-link ${utils.strIf(rounded, `nav-link-rounded`)}`}
-                         disabled={disabled}
-                         onClick={onClick}>
-            <MenuItem shrink={shrink}
-                      label={label}
-                      icon={icon}
-                      hoverAnimation={true}
-                      size={size}
-                      tooltip={tooltip}
-                      selected={selected}
-                      className={className}/>
+        <SensitiveButton 
+            className={`nav-link ${utils.strIf(rounded, `nav-link-rounded`)}`} 
+            disabled={disabled || label === "Updates"} // Optionally disable "Updates"
+            onClick={onClick}
+            data-label={label} // Added for CSS targeting
+        >
+            <MenuItem 
+                shrink={shrink}
+                label={label}
+                icon={icon}
+                hoverAnimation={true}
+                size={size}
+                tooltip={tooltip}
+                selected={selected}
+                className={className}
+            />
         </SensitiveButton>
-    )
+    );
 }
 
-export default NavLink
+export default NavLink;
